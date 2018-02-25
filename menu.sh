4426c3da2de34ba8a48ea79c4b429da0d2fa146d
Incrades <34095685+Incrades@users.noreply.github.com>
2018-02-25 22:55:32 +0700

    Add files via upload

---
+++ menu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Menu
+
+echo -e "* menu      : Menampilkan Daftar Perintah"
+echo -e "* usernew   : Membuat Account SSH & OpenVPN"
+echo -e "* trial     : Membuat Akun Trial"
+echo -e "* hapus     : Menghapus Akun SSH & OpenVPN"
+echo -e "* cek       : Cek Login User"
+echo -e "* member    : Daftar Member SSH & OpenVPN"
+echo -e "* resvis    : Restart Service All"
+echo -e "* reboot    : Reboot VPS"
+echo -e "* speedtest : Benchmark Speedtest VPS"
+echo -e "* info      : Menampilkan Informasi Sistem"
+echo -e "* about     : Info Script Auto Installer"
+echo -e "* exit      : Keluar Dari Aplikasi"
+echo -e ""
